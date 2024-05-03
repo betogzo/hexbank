4fd8e3f4569b8223ca1808bd758c4a98d3678b0a
betogzo <albertogaleazzo@msn.com>
2024-05-03 16:03:48 -0300

    feat(users): add basic user schema and validations

---
+++ lib/hex_bank/users/user.ex
@@ -0,0 +1,27 @@
+defmodule HexBank.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @required_params [:name, :password_hash, :email, :cep]
+
+  schema "users" do
+    field :name, :string
+    field :password_hash, :string
+    field :email, :string
+    field :cep, :string
+
+    timestamps()
+  end
+
+  # thats how ecto will communicate to the database
+  # what it will change and what will be changed as function params
+  # changeset can be used to both insertion and updating
+  def changeset(user \\ %__MODULE__{}, params) do # %__MODULE__{} == an empty map of HexBank.Users.User
+    user
+    |> cast(params, @required_params) # mapping what will be updated to what the app expects
+    |> validate_required(@required_params) # fields that are required, that can't be null
+    |> validate_length(:name, min: 3)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:cep, is: 8)
+  end
+end
