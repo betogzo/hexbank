d6bbccc0fc8edec7eda7637e579c9a75a2dfea3b
betogzo <albertogaleazzo@msn.com>
2024-05-03 14:46:41 -0300

    chore: initial commit

---
+++ test/hex_bank_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule HexBankWeb.ErrorJSONTest do
+  use HexBankWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert HexBankWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert HexBankWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
