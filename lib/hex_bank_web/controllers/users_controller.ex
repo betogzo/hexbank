36a100e7de988ebb22167fb1e19493debcac116b
betogzo <albertogaleazzo@msn.com>
2024-05-04 14:14:31 -0300

    refactor(controllers): ref users controller to use a fallback

--- lib/hex_bank_web/controllers/users_controller.ex
+++ lib/hex_bank_web/controllers/users_controller.ex
@@ -1,24 +1,17 @@
 defmodule HexBankWeb.UsersController do
   use HexBankWeb, :controller
 
-  alias HexBank.Users.Create
+  alias HexBank.Users.{User, Create}
+  alias HexBankWeb.FallbackController
+
+  action_fallback FallbackController
 
   def create(conn, params) do
-    params
-    |> Create.call()
-    |> handle_response(conn)
-  end
-
-  defp handle_response({:ok, user}, conn) do
-    conn
-    |> put_status(:created)
-    |> render(:create, user: user)
-  end
-
-  defp handle_response({:error, changeset}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(json: HexBankWeb.ErrorJSON)
-    |> render(:error, changeset: changeset)
+    # if Create.call(params) doesnt match {:ok, %User{} = user} the fallback will be called
+    with {:ok, %User{} = user} <- Create.call(params) do
+      conn
+      |> put_status(:created)
+      |> render(:create, user: user)
+    end
   end
 end
