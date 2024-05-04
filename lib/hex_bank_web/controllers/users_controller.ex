eb07ca1057fe333b2241ecace757e9a2d758a629
betogzo <albertogaleazzo@msn.com>
2024-05-04 14:38:05 -0300

    refactor(controllers): ref users contr. to use facade

--- lib/hex_bank_web/controllers/users_controller.ex
+++ lib/hex_bank_web/controllers/users_controller.ex
@@ -1,14 +1,15 @@
 defmodule HexBankWeb.UsersController do
   use HexBankWeb, :controller
 
-  alias HexBank.Users.{User, Create}
+  alias HexBank.Users
+  alias Users.User
   alias HexBankWeb.FallbackController
 
   action_fallback FallbackController
 
   def create(conn, params) do
     # if Create.call(params) doesnt match {:ok, %User{} = user} the fallback will be called
-    with {:ok, %User{} = user} <- Create.call(params) do
+    with {:ok, %User{} = user} <- Users.create(params) do
       conn
       |> put_status(:created)
       |> render(:create, user: user)
