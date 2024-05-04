1dca6c52a37cb9b15d5951768952539be7c5c66e
betogzo <albertogaleazzo@msn.com>
2024-05-04 11:12:57 -0300

    feat(controller): add users_controller and create handler

---
+++ lib/hex_bank_web/controllers/users_controller.ex
@@ -0,0 +1,17 @@
+defmodule HexBankWeb.UsersController do
+  use HexBankWeb, :controller
+
+  alias HexBank.Users.Create
+
+  def create(conn, params) do
+    params
+    |> Create.call()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, user}, conn) do
+    conn
+    |> put_status(:created)
+    |> render(:create, user: user)
+  end
+end
