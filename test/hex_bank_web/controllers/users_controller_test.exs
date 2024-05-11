8240ca40766b7c749de3ae51fba8fd94e7c80c38
betogzo <albertogaleazzo@msn.com>
2024-05-11 12:40:00 -0300

    tests(controllers): mock viacep service on user controller tests

--- test/hex_bank_web/controllers/users_controller_test.exs
+++ test/hex_bank_web/controllers/users_controller_test.exs
@@ -1,17 +1,43 @@
 defmodule HexBankWeb.UsersControllerTest do
   use HexBankWeb.ConnCase
 
+  import Mox
+
   alias HexBank.Users
   alias Users.User
 
+  setup do
+    params = %{
+      "name" => "Beto",
+      "cep" => "87300005",
+      "password" => "12345",
+      "email" => "teste@email.com"
+    }
+
+    body = %{
+      "bairro" => "Centro",
+      "cep" => "87300-005",
+      "complemento" => "de 1191/1192 a 2519/2520",
+      "ddd" => "44",
+      "gia" => "",
+      "ibge" => "4104303",
+      "localidade" => "Campo Mourão",
+      "logradouro" => "Avenida Capitão Índio Bandeira",
+      "siafi" => "7483",
+      "uf" => "PR"
+    }
+
+    {:ok, %{user_params: params, body: body}}
+  end
+
   describe "create/2" do
-    test "successfully creates an user", %{conn: conn} do
-      params = %{
-        "name" => "Beto",
-        "cep" => "87300005",
-        "password" => "12345",
-        "email" => "teste@email.com"
-      }
+    test "successfully creates an user", %{
+      conn: conn,
+      user_params: params,
+      body: body
+    } do
+      # mocking viacep response on user creation
+      expect(HexBank.ViaCep.ClientMock, :call, fn "87300005" -> {:ok, body} end)
 
       response =
         conn
@@ -27,10 +53,12 @@ defmodule HexBankWeb.UsersControllerTest do
     test "returns an error when invalid params are provided", %{conn: conn} do
       params = %{
         name: nil,
-        cep: "87300005",
+        cep: "12",
         password: "12345",
         email: "teste@email.com"
       }
+
+      expect(HexBank.ViaCep.ClientMock, :call, fn "12" -> {:ok, ""} end)
 
       response =
         conn
@@ -45,13 +73,12 @@ defmodule HexBankWeb.UsersControllerTest do
   end
 
   describe "delete/2" do
-    test "successfully deletes an user when a valid id is provided", %{conn: conn} do
-      params = %{
-        "name" => "Beto",
-        "cep" => "87300005",
-        "password" => "12345",
-        "email" => "teste@email.com"
-      }
+    test "successfully deletes an user when a valid id is provided", %{
+      conn: conn,
+      user_params: params,
+      body: body
+    } do
+      expect(HexBank.ViaCep.ClientMock, :call, fn "87300005" -> {:ok, body} end)
 
       {:ok, %User{id: id}} = Users.create(params)
 
