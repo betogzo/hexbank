d12135c5cf44331419a57caedbad64fb915e7185
betogzo <albertogaleazzo@msn.com>
2024-05-10 22:27:19 -0300

    feat(users): add cep validation on user creation

--- lib/hex_bank/users/create.ex
+++ lib/hex_bank/users/create.ex
@@ -1,10 +1,13 @@
 defmodule HexBank.Users.Create do
   alias HexBank.Users.User
   alias HexBank.Repo
+  alias HexBankWeb.ViaCep.Client, as: ViaCepClient
 
-  def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+  def call(%{"cep" => cep} = params) do
+    with {:ok, _result} <- ViaCepClient.call(cep) do
+      params
+      |> User.changeset()
+      |> Repo.insert()
+    end
   end
 end
