c96a14da207aef969d96fc0899c3008f66a0c012
betogzo <albertogaleazzo@msn.com>
2024-05-11 09:50:39 -0300

    chore(deps): add bypass dep

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule HexBank.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
       {:argon2_elixir, "~> 4.0"},
-      {:tesla, "~> 1.9"}
+      {:tesla, "~> 1.9"},
+      {:bypass, "~> 2.1", only: :test}
     ]
   end
 
