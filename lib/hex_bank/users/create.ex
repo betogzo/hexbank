49b871b80a465a6fe45bd7dbb213b5e0edb4562c
betogzo <albertogaleazzo@msn.com>
2024-05-11 11:53:02 -0300

    feat(users): add mock config

--- lib/hex_bank/users/create.ex
+++ lib/hex_bank/users/create.ex
@@ -4,10 +4,17 @@ defmodule HexBank.Users.Create do
   alias HexBank.ViaCep.Client, as: ViaCepClient
 
   def call(%{"cep" => cep} = params) do
-    with {:ok, _result} <- ViaCepClient.call(cep) do
+    with {:ok, _result} <- client().call(cep) do
       params
       |> User.changeset()
       |> Repo.insert()
     end
+  end
+
+  # works similar to dependency injection depending on the environment
+  # when in development environment, it will serve what we defined on /test/test_helper.exs
+  # when in production, it will use ViaCepClient
+  defp client() do
+    Application.get_env(:hex_bank, :via_cep_client, ViaCepClient)
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,5 @@
+Mox.defmock(HexBank.ViaCep.ClientMock, for: HexBank.ViaCep.ClientBehaviour)
+Application.put_env(:hex_bank, :via_cep_client, HexBank.ViaCep.ClientMock)
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(HexBank.Repo, :manual)
