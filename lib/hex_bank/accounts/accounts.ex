f5c9ed4cc07d22c278c620fc1816ce5317ba96eb
betogzo <albertogaleazzo@msn.com>
2024-05-13 22:01:56 -0300

    feat(accounts): deal with transaction result and change params type

--- lib/hex_bank/accounts/accounts.ex
+++ lib/hex_bank/accounts/accounts.ex
@@ -3,5 +3,5 @@ defmodule HexBank.Accounts do
   alias HexBank.Accounts.Transaction
 
   defdelegate create(params), to: Create, as: :call
-  defdelegate transaction(from_account_id, to_account_id, value), to: Transaction, as: :call
+  defdelegate transaction(params), to: Transaction, as: :call
 end
--- lib/hex_bank/accounts/transaction.ex
+++ lib/hex_bank/accounts/transaction.ex
@@ -3,7 +3,7 @@ defmodule HexBank.Accounts.Transaction do
   alias HexBank.Repo
   alias Ecto.Multi
 
-  def call(from_acc_id, to_acc_id, value) do
+  def call(%{"from_account_id" => from_acc_id, "to_account_id" => to_acc_id, "value" => value}) do
     with %Account{} = from_acc <- Repo.get(Account, from_acc_id),
          %Account{} = to_acc <- Repo.get(Account, to_acc_id),
          {:ok, value} <- Decimal.cast(value) do
@@ -11,11 +11,14 @@ defmodule HexBank.Accounts.Transaction do
       |> withdraw(from_acc, value)
       |> deposit(to_acc, value)
       |> Repo.transaction()
+      |> handle_transaction_result()
     else
       nil -> {:error, :not_found}
       :error -> {:error, :bad_request}
     end
   end
+
+  def call(_), do: {:error, :bad_request}
 
   defp withdraw(multi, from_acc, value) do
     new_balance = Decimal.sub(from_acc.balance, value)
@@ -28,4 +31,7 @@ defmodule HexBank.Accounts.Transaction do
     changeset = Account.changeset(to_acc, %{balance: new_balance})
     Multi.update(multi, :deposit, changeset)
   end
+
+  defp handle_transaction_result({:ok, _result} = result), do: result
+  defp handle_transaction_result({:error, _1, reason, _2}), do: {:error, reason}
 end
