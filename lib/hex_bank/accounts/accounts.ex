47112e3f5c2e620e88558d26055efa5aaf345546
betogzo <albertogaleazzo@msn.com>
2024-05-13 21:34:22 -0300

    feat(accounts): add delegate function to transaction facade

--- lib/hex_bank/accounts/accounts.ex
+++ lib/hex_bank/accounts/accounts.ex
@@ -1,5 +1,7 @@
 defmodule HexBank.Accounts do
   alias HexBank.Accounts.Create
+  alias HexBank.Accounts.Transaction
 
   defdelegate create(params), to: Create, as: :call
+  defdelegate transaction(from_account_id, to_account_id, value), to: Transaction, as: :call
 end
