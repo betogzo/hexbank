bce8534f76637da454c95709de3460e7ae8fd51d
betogzo <albertogaleazzo@msn.com>
2024-05-13 22:01:03 -0300

    feat(controllers): add successfull transaction to accounts_json

--- lib/hex_bank_web/controllers/accounts_json.ex
+++ lib/hex_bank_web/controllers/accounts_json.ex
@@ -1,9 +1,16 @@
 defmodule HexBankWeb.AccountsJSON do
-
   def create(%{account: account}) do
     %{
       status: "ok",
       data: account
+    }
+  end
+
+  def transaction(%{transaction: %{withdraw: from_account, deposit: to_account}}) do
+    %{
+      status: "ok",
+      from_account: from_account.id,
+      to_account: to_account.id
     }
   end
 end
