a479f3145c2eea002c9000692d138583e353a8b7
betogzo <albertogaleazzo@msn.com>
2024-05-16 22:47:37 -0300

    feat(users): add delegate to login func on users facade

--- lib/hex_bank/users.ex
+++ lib/hex_bank/users.ex
@@ -1,6 +1,5 @@
 # file to implement the facade design pattern on the users context
 defmodule HexBank.Users do
-
   # HexBank.Users.create will actually call the function HexBank.Users.Create.call
   defdelegate create(params), to: HexBank.Users.Create, as: :call
 
@@ -9,4 +8,6 @@ defmodule HexBank.Users do
   defdelegate delete(id), to: HexBank.Users.Delete, as: :call
 
   defdelegate update(params), to: HexBank.Users.Update, as: :call
+
+  defdelegate login(params), to: HexBank.Users.Verify, as: :call
 end
